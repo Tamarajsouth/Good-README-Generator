68d8f96ec2502eb8459f98f008a7779ce2614037
tamara south <tjeansouth@gmail.com>
2020-04-09 17:37:29 -0400

    reconfigured generateMarkdown.js and added parameters for future questions array

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Good-README-Generator
 A command-line application that dynamically generates a README.md from a user's input
+
--- index.js
+++ index.js
@@ -4,29 +4,47 @@ const axios = require("axios");
 
 const questions = ["What is your GitHub username?"];
 
-function writeToFile(fileName, data) {}
+function generateMarkdown(generateMarkdown, data) {   
+  return `
+  # Good-README-Generator
+  A command-line application that dynamically generates a README.md from a user's input
+  ${answers.name}
+  ${answers.email}
+  ${answers.image}
+  ${answers.title}
+  ${answers.description}
+  ${answers.tableOfContents}
+  ${answers.installation}
+  ${answers.usage}
+  ${answers.license}
+  ${answers.contributing}
+  ${answers.tests}
+  ${answers.questions}
+  `
+}
 
 async function init() {
-    let userName = await
-        inquirer
-            .prompt([
-                {
-                    type: "input",
-                    message: questions[0],
-                    name: "username"
-                }
-            ]).then((response) => response.username);
-    
-        await axios.get(`https://api.github.com/users/${userName}`)
-        .then((response) => console.log(response.data));
-        
-        fs.appendFile("log.txt", userName, err => {
-            if(err){
-                console.log(err)
-            }else{
-                console.log("success!")
-            }
-        })
+  let userName = await inquirer
+    .prompt([
+      {
+        type: "input",
+        message: questions[0],
+        name: "username",
+      },
+    ])
+    .then((response) => response.username);
+
+  await axios
+    .get(`https://api.github.com/users/${userName}`)
+    .then((response) => console.log(response.data));
+
+  fs.appendFile("log.txt", userName, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Success!");
+    }
+  });
 }
 
 init();
