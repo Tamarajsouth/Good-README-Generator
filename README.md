2453faddddf0634cf78f203d429b98c249cca398
tamara south <tjeansouth@gmail.com>
2020-04-10 16:59:25 -0400

    got array of questions to cycle through, writing readme.md file successful, now to test how it looks on the other end

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-# Good-README-Generator
-A command-line application that dynamically generates a README.md from a user's input
+# kdkdk README 
 
-tamarajsouth
+      ### **Tamara South 
+
+      papapa 
+
+      ![user picture](https://avatars2.githubusercontent.com/u/60523597?v=4) 
+
+      ## ** Table of Contents ** 
+
+      ## ** Install Guide ** 
+
+      mamamam 
+
+      ## ** Usage ** 
+
+      nenenene 
+
+      ## ** License ** 
+
+      MIT 
+
+      ## ** Contributors ** 
+
+      oaoaoao 
+
+      ## ** Tests ** 
+
+      lelele 
+
+      ## ** Questions ** 
+
+      papap 
+
+      
+    
--- index.js
+++ index.js
@@ -2,49 +2,123 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const axios = require("axios");
 
-const questions = ["What is your GitHub username?"];
+const userQuestions = ["What is your GitHub username?", "Title:", "Description:", "Table of Contents:", "Istallation:",
+"Usage:", "Licenses:", "Contributing:", "Tests:", "Questions:"];
 
-function generateMarkdown(generateMarkdown, data) {   
-  return `
-  # Good-README-Generator
-  A command-line application that dynamically generates a README.md from a user's input
-  ${answers.name}
-  ${answers.email}
-  ${answers.image}
-  ${answers.title}
-  ${answers.description}
-  ${answers.tableOfContents}
-  ${answers.installation}
-  ${answers.usage}
-  ${answers.license}
-  ${answers.contributing}
-  ${answers.tests}
-  ${answers.questions}
-  `
-}
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, err => {
+      if (err) {
+          console.log(err)
+      } else {
+          console.log("Success");
+      }
+  })
+}  
 
 async function init() {
-  let userName = await inquirer
+  let userName;
+  let appTitle;
+  let appDescription;
+  let tableOfContents;
+  let install;
+  let usage;
+  let license;
+  let contributing;
+  let tests;
+  let questions;
+   await inquirer
     .prompt([
       {
         type: "input",
-        message: questions[0],
+        message: userQuestions[0],
         name: "username",
       },
+      {
+        type: "input",
+        message: userQuestions[1],
+        name: "title",
+      },
+      {
+        type: "input",
+        message: userQuestions[2],
+        name: "description",
+      },
+      {
+        type: "input",
+        message: userQuestions[3],
+        name: "tableOfContents",
+      },
+      {
+        type: "input",
+        message: userQuestions[4],
+        name: "install",
+      },
+      {
+        type: "input",
+        message: userQuestions[5],
+        name: "usage",
+      },
+      {
+        type: "checkbox",
+        message: userQuestions[6],
+        name: "license", 
+        choices: ["MIT", "GPLv3", "AGPL"] 
+      },
+      {
+        type: "input",
+        message: userQuestions[7],
+        name: "contributing",
+      },
+      {
+        type: "input",
+        message: userQuestions[8],
+        name: "tests",
+      },
+      {
+        type: "input",
+        message: userQuestions[9],
+        name: "questions",
+      },
     ])
-    .then((response) => response.username);
+    .then((response) => {
+    userName = response.username;
+    appTitle = response.title;
+    appDescription = response.description;
+    tableOfContents = response.tableOfContents;
+    install = response.install;
+    usage = response.usage;
+    license = response.license;
+    contributing = response.contributing;
+    tests = response.tests;
+    questions = response.questions;
+    });
 
   await axios
     .get(`https://api.github.com/users/${userName}`)
-    .then((response) => console.log(response.data));
-
-  fs.appendFile("README.md", userName, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Success!");
-    }
-  });
+    .then((response) => {
+      const generatedMarkdown =
+      `# ${appTitle} README \n
+      ### **${response.data.name} \n
+      ${appDescription} \n
+      ![user picture](${response.data.avatar_url}) \n
+      ## ** Table of Contents ** \n
+      ## ** Install Guide ** \n
+      ${install} \n
+      ## ** Usage ** \n
+      ${usage} \n
+      ## ** License ** \n
+      ${license} \n
+      ## ** Contributors ** \n
+      ${contributing} \n
+      ## ** Tests ** \n
+      ${tests} \n
+      ## ** Questions ** \n
+      ${questions} \n
+      
+    ` 
+  writeToFile("README.md", generatedMarkdown);
+});
 }
 
 init();
